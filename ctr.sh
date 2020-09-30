69c6dc69aaa83430157faaf59a008e42e0dd778e
ctrgaming <72120674+ctrgaming@users.noreply.github.com>
2020-09-30 07:25:50 +0000

    Create ctr.sh

---
+++ ctr.sh
@@ -0,0 +1,70 @@
+clear
+sleep 2
+echo"──────────────────────"
+echo"   📂 MENU TOOLS IS BACK 📂         "
+echo"   ✔ FB = Faizul ✔                               "
+echo"   📂 WA = 082271426251 📂                 "
+echo"──────────────────────"
+
+
+echo $i"─────────────────────────────────────"
+  echo $i"|"$me" 1"$i" |"$cy" BRUTEFORCE FB PHP✔        "$i"|"$i"    |"$cy" B"$i" |"
+  echo $i"|"$me" 2"$i" |"$cy" clon akun luar negri✔    "$i"|"$i"    |"$cy" E"$i" |"
+  echo $i"|"$me" 3"$i" |"$cy" spam wattsapps❌    "$i"|"$i"    |"$cy" N"$i" |"
+  echo $i"|"$me" 4"$i" |"$cy" virus android✔       "$i"|"$i"    |"$cy" G"$i" |"
+  echo $i"|"$me"19"$i" |"$cy" KELUAR❌/EXIT PROGRAM      "$i"|"$i"    |"$cy" N"$i" |"
+  echo $i"─────────────────────────────────────"
+  echo
+  echo $me"┌==="$bi"["$i"FR13ND8"$bi"]"$me"======"$bi"["$i""Pilih Nomornya""$bi"]"
+  echo $me"¦"
+  read -p"└──# " pil
+  
+  if [ $pil = 1 ]
+  then
+  clear
+  figlet -f slant "W A I T"|lolcat
+  sleep 1
+  git clone https://github.com/FR13ND8/fbbrute
+  cd fbbrute
+  php fb.php
+  fi
+
+if [ $pil =  1 ]
+then
+clear
+figlet -f slant" mengginstall" | lolcat
+sleep 1
+git clone https://github.com/BOT-033/Sensei
+cd Sensei
+python2  main.py
+fi
+
+if [ $pil = 1 ]
+then
+clear
+figlet -f slant"menggintasall" | lolcat
+sleep 1
+git clone https://github.com/IL4NGQW3R/spamwa
+cd spamwa
+python spamwa-22.py
+fi
+
+if [ $pil = 1 ]
+then
+clear
+figlet -f slant"mengginstall" | lolcat
+sleep 2
+git clone https://github.com/rzl2018/BCA-X666X
+cd BCA-X666X
+python2 B-VIRUS.py
+fi
+
+
+if [ $pil = 19 ]
+ then
+ clear
+ figlet -f slant "E X I T"|lolcat
+ sleep 3
+echo" TERIMAKASI SUDAH MENGGUNAKAN SCRIP INI"
+echo"     📂FB FAIZUL📂   "
+echo"     📂 WA  : 082271426251 📂 "
